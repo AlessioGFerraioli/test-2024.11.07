d46e3682ced1e487b1c0028ba8aae369e3a5e4b8
Alessio Giuseppe Ferraioli <alessioferraioli@hotmail.it>
2024-11-07 16:38:38 +0100

    Update es5.py

--- es5.py
+++ es5.py
@@ -5,9 +5,17 @@ es5
 
 
 
+from abc import ABC, abstractmethod
+
 
 class Professore:
     pass
 
-class Studente:
+class Materia(ABC)
+    pass
+
+class Aula(Professore, Materia):
+    pass
+
+class Studente():
     pass
